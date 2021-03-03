22f5adbcf5e8a80392d65ab6e7102350f6e2fdc2
gdrayton001 <47512642+gdrayton001@users.noreply.github.com>
2021-03-02 22:49:18 -0600

    create DBController

---
+++ src/CMC/user/DBController.java
@@ -0,0 +1,20 @@
+package CMC.user;
+
+public class DBController {
+
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
